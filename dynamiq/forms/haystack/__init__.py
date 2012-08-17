4c5defe0cdfda4c55788eb7c18edfd22f034edab
Yohan Boniface <yb@enix.org>
2012-08-18 00:51:31 +0200

    Define filter constants at class level, to use from classes

--- dynamiq/forms/haystack/__init__.py
+++ dynamiq/forms/haystack/__init__.py
@@ -7,10 +7,12 @@ from .constants import (FILTER_LOOKUPS_FULLTEXT, FILTER_LOOKUPS)
 
 class HaystackForm(DynamiqAdvancedForm):
 
+    # Redefine it class level
+    FILTER_LOOKUPS_FULLTEXT = FILTER_LOOKUPS_FULLTEXT
+    FILTER_LOOKUPS = FILTER_LOOKUPS
+
     def __init__(self, *args, **kwargs):
         super(HaystackForm, self).__init__(*args, **kwargs)
 
         # Fulltext lookups are different between SeSQL and Haystack
-        self.FILTER_LOOKUPS_FULLTEXT = FILTER_LOOKUPS_FULLTEXT
-        self.FILTER_LOOKUPS = FILTER_LOOKUPS
         self.fields['fulltext_lookup'].extended_choices = self.FILTER_LOOKUPS_FULLTEXT
--- dynamiq/forms/sesql/__init__.py
+++ dynamiq/forms/sesql/__init__.py
@@ -30,10 +30,12 @@ class SeSQLOptionsForm(DynamiqSearchOptionsForm):
 
 class SeSQLForm(DynamiqAdvancedForm):
 
+    # Redefine it at class level
+    FILTER_LOOKUPS_FULLTEXT = FILTER_LOOKUPS_FULLTEXT
+    FILTER_LOOKUPS = FILTER_LOOKUPS
+
     def __init__(self, *args, **kwargs):
         super(SeSQLForm, self).__init__(*args, **kwargs)
 
         # Fulltext lookups are different between SeSQL and Haystack
-        self.FILTER_LOOKUPS_FULLTEXT = FILTER_LOOKUPS_FULLTEXT
-        self.FILTER_LOOKUPS = FILTER_LOOKUPS
         self.fields['fulltext_lookup'].extended_choices = self.FILTER_LOOKUPS_FULLTEXT
