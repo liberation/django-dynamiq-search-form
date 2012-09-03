dfaf180070155b481c34d6bb8d92ce115a35dc46
Yohan Boniface <yb@enix.org>
2012-09-04 00:08:59 +0200

    Implementing DynamiqBaseFormsetTests

--- dynamiq/tests/base.py
+++ dynamiq/tests/base.py
@@ -1,7 +1,64 @@
 from django.utils.unittest import TestCase
 
+from ..utils import get_advanced_search_formset_class, FiltersBuilder
+
 
 class DynamiqBaseTests(TestCase):
 
     def assertEqualQ(self, Q1, Q2):
         self.assertEqual(str(Q1), str(Q2))
+
+
+class DynamiqBaseFormsetTests(DynamiqBaseTests):
+    """
+    Inherite from this class for testing formsets.
+    Just define formset and form.
+    And use run_test to test your datas.
+    """
+
+    formset = None
+    form = None
+
+    def run_test(self, data, expected):
+        """
+        `data` here is a list of dicts, each reprensenting the data of a form
+        of the formset.
+        Ex.:
+        data = [{
+            'filter_name': 'captain',
+            'fulltext_lookup': 'exact',
+            'filter_value_fulltext': u'Tabarly',
+        }]
+        `expected` is a Q object.
+        """
+        data = self.build_data(data)
+        formset_class = get_advanced_search_formset_class(None, self.formset, self.form)
+        formset = formset_class(data)
+        formset.full_clean()
+        if formset.is_valid():
+            F = FiltersBuilder(formset)
+            query, label = F()
+        else:
+            self.fail(formset.errors)
+        self.assertEqualQ(query, expected)
+
+    def build_data(self, data):
+        """
+        Build data needed to instantiate a formset from a set of form datas.
+        """
+        formset_data = {
+            'form-TOTAL_FORMS': 0,  # Will be updated at the end
+            'form-INITIAL_FORMS': 0,
+            'form-MAX_NUM_FORMS': '',
+            'sort': self.formset.options_form_class.SORT.YEAR,
+            'limit': 15,
+        }
+        total_form = 0
+        for idx, form_data in enumerate(data):
+            total_form += 1
+            for key, value in form_data.iteritems():
+                formset_data.update({
+                    'form-%d-%s' % (idx, key): value
+                })
+        formset_data['form-TOTAL_FORMS'] = total_form
+        return formset_data
--- dynamiq/tests/utils.py
+++ dynamiq/tests/utils.py
@@ -3,9 +3,10 @@
 from django.db.models import Q
 from django.core.exceptions import ValidationError
 
+
 from ..utils import StringFiltersBuilder
-from .forms.haystack import BoatSearchForm
-from .base import DynamiqBaseTests
+from .forms.haystack import BoatSearchForm, BoatSearchAdvancedFormset
+from .base import DynamiqBaseTests, DynamiqBaseFormsetTests
 
 
 class StringFiltersBuilderTests(DynamiqBaseTests):
@@ -180,3 +181,54 @@ class StringFiltersBuilderTests(DynamiqBaseTests):
         F = StringFiltersBuilder(q, BoatSearchForm, raise_on_error=True)
         with self.assertRaises(ValidationError):
             query, label = F()
+
+
+class FiltersBuilderTests(DynamiqBaseFormsetTests):
+
+    formset = BoatSearchAdvancedFormset
+    form = BoatSearchForm
+
+    def test_simple_fulltext_search(self):
+        data = [
+            {
+                'filter_name': 'captain',
+                'fulltext_lookup': BoatSearchForm.FILTER_LOOKUPS_FULLTEXT.CONTAINS,
+                'filter_value_fulltext': u'Tabarly',
+            }
+        ]
+        expected = Q(captain__contains=u"Tabarly")
+        self.run_test(data, expected)
+
+    def test_filters_can_be_ANDed(self):
+        data = [
+            {
+                'filter_name': 'captain',
+                'fulltext_lookup': BoatSearchForm.FILTER_LOOKUPS_FULLTEXT.CONTAINS,
+                'filter_value_fulltext': u'Tabarly',
+                'filter_right_op': BoatSearchForm.FILTER_RIGHT_OP.AND,
+            },
+            {
+                'filter_name': 'mast',
+                'int_lookup': BoatSearchForm.FILTER_LOOKUPS_INT.EXACT,
+                'filter_value_int': 1,
+            }
+        ]
+        expected = Q(captain__contains=u"Tabarly") & Q(mast=1)
+        self.run_test(data, expected)
+
+    def test_filters_can_be_ORed(self):
+        data = [
+            {
+                'filter_name': 'captain',
+                'fulltext_lookup': BoatSearchForm.FILTER_LOOKUPS_FULLTEXT.CONTAINS,
+                'filter_value_fulltext': u'Tabarly',
+                'filter_right_op': BoatSearchForm.FILTER_RIGHT_OP.OR,
+            },
+            {
+                'filter_name': 'captain',
+                'fulltext_lookup': BoatSearchForm.FILTER_LOOKUPS_FULLTEXT.CONTAINS,
+                'filter_value_fulltext': u'Moitessier',
+            }
+        ]
+        expected = Q(captain__contains=u"Tabarly") | Q(captain__contains=u"Moitessier")
+        self.run_test(data, expected)
